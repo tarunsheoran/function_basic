7dd338775c4e6183420b82ae9b2eb363393414d9
Tarun Sheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-10-16 11:36:31 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+void myfunction(){
+    cout<<"we are leaning function";
+}
+void func(){
+    int x, y;
+    cout<<"Enter your number here";
+    cin>>x>>y;
+    cout<<x+y;
+};
+
+
+int main(){
+    myfunction();
+    func();
+}
+
+
